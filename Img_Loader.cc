5fd3c09bf8e14c57d90d5b3bbda96640f3995f47
GuillaumeGas <gas.guillaume@free.fr>
2014-03-12 14:51:39 +0100

    Ajout de get_high_map

--- Img_Loader.cc
+++ Img_Loader.cc
@@ -3,21 +3,25 @@
 using namespace std;
 
 Img_Loader::Img_Loader(string file) {
-  SDL_Surface * surf = NULL;
-  surf = SDL_LoadBMP(file.c_str());
+    SDL_Surface * surf = NULL;
+    surf = SDL_LoadBMP(file.c_str());
 
-  if(!surf) {
-    cout << "Erreur !" << endl;
-  } else {
-    for(int i = 0; i < surf->w; i++) {
-      high_map.push_back(vector<int>());
-      for(int j = 0; j < surf->h; j++) {
-	Uint8 r, g, b;
-	SDL_GetRGB(((Uint32*)surf->pixels)[i + j * surf->w], surf->format, &r, &g, &b);
-	high_map[i].push_back(r);
-      }
+    if(!surf) {
+	cout << "Erreur !" << endl;
+    } else {
+	for(int i = 0; i < surf->w; i++) {
+	    high_map.push_back(vector<int>());
+	    for(int j = 0; j < surf->h; j++) {
+		Uint8 r, g, b;
+		SDL_GetRGB(((Uint32*)surf->pixels)[i + j * surf->w], surf->format, &r, &g, &b);
+		high_map[i].push_back(r);
+	    }
+	}
     }
-  }
 
   
+}
+
+vector<vector<int>> Img_Loader::get_high_map() {
+    return high_map;
 }

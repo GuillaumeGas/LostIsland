66af79de9f0ee2db530ebef49688eaecc7ccd0d9
EmileCadorel <ecadorel@gmail.com>
2014-03-22 19:42:30 +0100

    mise en place d'un brouillard un peu plus fort pour renforcer les effets

--- Engine.cpp
+++ Engine.cpp
@@ -17,12 +17,12 @@ void Engine::init(string titre, int w, int h) {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(70, (double)h/w, 1, 100000);
-  //  glEnable(GL_FOG) ;
+  glEnable(GL_FOG) ;
   GLfloat fogcolor[4] = {0.5, 0.5, 0.5, 1} ;
   GLint fogmode = GL_EXP ;
   glFogi (GL_FOG_MODE, fogmode) ;
   glFogfv(GL_FOG_COLOR, fogcolor) ;
-  glFogf(GL_FOG_DENSITY, 0.0005) ;
+  glFogf(GL_FOG_DENSITY, 0.005) ;
   glFogf(GL_FOG_START, 1.0) ;
   glFogf(GL_FOG_END, 5.0) ;
   glEnable(GL_DEPTH_TEST);

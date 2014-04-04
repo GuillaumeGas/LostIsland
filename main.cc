5218307ad859f5a3f9e6d1dd60c68cd2a9f9979b
EmileCadorel <ecadorel@gmail.com>
2014-04-04 09:25:58 +0200

    modification nom main.cc

--- main.cc
+++ main.cc
@@ -1,231 +1,252 @@
-#include <GL/glut.h>
-
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <ctime>
-
-#include "High_Calc.hh"
+#include "Engine.hpp"
+#include "Event.hpp"
 #include "Img_Loader.hh"
+#include "High_Calc.hh"
+#include <iostream>
+#include <math.h>
+#include <GL/glut.h>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include "Cube.hpp"
+#include "Tree.hpp"
+#include "ObjectOff.hpp"
 
 using namespace std;
 
-Img_Loader c( "img.jpg" );
-High_Calc h( c.get_high_map() );
-
-int angle = 0;
-
-void MyReDisplay(void)
-{
-  glutPostRedisplay();
-
-}
-
-
-
-void framerate(void){
-  static int frame=0,time,timebase=0;
+string framerate() {
+ static int frame=0,time,timebase=0;
   static char titre[100];
 	
   frame++;
-  time=glutGet(GLUT_ELAPSED_TIME);
+  time = SDL_GetTicks();
   if (time - timebase > 1000) {
-    sprintf(titre,"FPS:%4.2f",
-	    frame*1000.0/(time-timebase));
-    glutSetWindowTitle(titre);	
-    timebase = time;		
-    frame = 0;
+      sprintf(titre,"FPS:%4.2f",frame*1000.0/(time-timebase));
+      timebase = time;		
+      frame = 0;
   }
+  return string(titre);
+}
+
+
+
+
+void load(vector<float> &somm, vector<int> & ind, const char * file ) {
+   ifstream f(file, ios::in);
+    string ch;
+    int aux;
+    int nbSommets, nbIndex;
+    
+    f >> ch >> nbSommets >> nbIndex >> aux;
+
+    if(ch == "OFF") {
+	int j = 0;
+	for(int i = 0; i < nbSommets; i++) {
+	    float a,b,c;
+	    f >> a >> b >> c;
+	    somm.push_back(a);
+	    somm.push_back(b);
+	    somm.push_back(c);
+	}
 	
-}	
+	int nb;
+	f >> nb;
+	j = 0;
+	for(int i = 0; i < nbIndex; i++) {
+	    for(int k = 0; k < nb; k++) {
+		int a;
+		f >> a;
+		ind.push_back(a);
+	    }
+	    f >> nb;
+	}
+    } else {
+	cout << "Erreur, fichier inconnu." << endl;
+	throw -1;
+    }
+    f.close();
+}
 
 
 
-void dessine() {
-    glColor3d(1,1,1);
+void dessine(High_Calc & h) {
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
-    //glTexCoordPointer(3, GL_FLOAT, 0, h.get_point());
     glColorPointer(3, GL_FLOAT, 0, h.get_color());
     glVertexPointer ( 3, GL_FLOAT, 0, h.get_point() );
     int w = h.get_w();
-    for ( int i = 0 ; i < w - 1 ; i++) { 
+    for ( int i = 0 ; i < w - 1 ; i++) {
 	glDrawElements( GL_TRIANGLE_STRIP , h.get_w()*2 , GL_UNSIGNED_INT, &h.get_vertex()[w * 2 * i]);
+
+    }    
+    glDisableClientState(GL_COLOR_ARRAY);
+}
+
+
+
+string load_args(int argc, char ** argv) {
+    if ( argc != 1 ) {
+	return argv[1];
+    } else {
+	return "IMG/sauv1.jpg";
+    }
+}
+
+
+int main(int argc, char ** argv) {
+    srand(time(NULL));
+    string img = load_args(argc, argv);
+    Img_Loader c(img.c_str());
+    int zoom_x = 10.0, zoom_z = 10.0, zoom_y = 1.0;
+    High_Calc h(c.get_high_map(), zoom_x, zoom_z, zoom_y);
+
+    int MatSpec[] = {0,0,0,0};
+    Engine en;
+    int largeur = 1000, hauteur = 768;
+    en.init("test", largeur, hauteur);
+    int _left = -10, up = 0, front = -50;
+    en.getCamera()->setLook(100,100,100,80,-10,80,0,1,0); 
+
+    vector <float> position;
+    for ( int i = 0 ; i < 100 * 3 ; i+=3 ) {
+	double x, y, z;
+	do {
+	    x = (double)(rand()%h.get_w()*zoom_x+1);
+	    z = (double)(rand()%h.get_w()*zoom_z+1);
+	    y = h.get_high(x, z) + 5;
+	} while(y <= 10 || y >= 90);
+	position.push_back(x);
+	position.push_back(y);
+	position.push_back(z);
+
     }
 
+
+    vector<Tree> vec_tree;
+    for(int i = 0; i < 100; i++) {
+	double x, y, z, he, wi;
+	do {
+	    x = (double)(rand()%h.get_w() + 1);
+	    z = (double)(rand()%h.get_w() + 1);
+	    y = h.get_high(x * zoom_x, z * zoom_z) - 5;
+	    he = (double)(rand()%150+50);
+	} while(y <= 10 || y >= 90);
+	vec_tree.push_back(Tree(x, y, z, he, 0.6, 0.6, zoom_x, zoom_y, zoom_z));
+    }
     
+    ObjectOff obsf("maillages/triceratops.off");
 
-}
+    Event e;
+    int i = 0;
+    GLfloat dif[] = {1.0,1.0,1.0,1.0};
+    bool retour = false;
+    int LightPos[] = {0,100 , 0,1};
+    bool retour_lum = false;
+    e[LEFT_CL] = 0;
+    while (!e[QUIT] ) {
+	i++;
+	e.UpdateEvent();
+	glMaterialiv(GL_FRONT_AND_BACK,GL_SPECULAR,MatSpec);
+	glMaterialiv(GL_FRONT_AND_BACK,GL_AMBIENT,MatSpec);
+	glMateriali(GL_FRONT_AND_BACK,GL_SHININESS,10); 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-
-
-double dist = -10, hauteur;
-int _left = -10, up = 0, front = -50;
-
-void display(void)
-{
-    framerate();
-  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ); //vide r√àellement la fen√çtre
-  glLoadIdentity();
-  glColor3f(1.0,1.0,1.0);
-  gluLookAt(100,100,100,80, -10 ,80,0,1,0);
-  glTranslated ( _left,up,front);
-  glRotated(angle, 0,1,0);
-  dessine();
-  glutSwapBuffers();
-}
-
-
-
-GLubyte CheckImage0[40][40][4];
-
-void makeCheckImage() {
-    GLubyte c;
-    for(int i = 0 ; i < 40 ; i++ ) {
-	for ( int j = 0 ; j < 40 ; j++ ) {
-	    c = rand()%255;
-	    CheckImage0[i][j][0] = rand()%255;
-	    CheckImage0[i][j][1] = rand()%255;
-	    CheckImage0[i][j][2] = rand()%255;
-	    
-	    CheckImage0[i][j][3] = 200;
+	if ( i >  5) {
+	    e.setMousePos(largeur/2, hauteur/2);
+	    i=0;
 	}
+	if ( e[LEFT_CL] ) {
+	    glMatrixMode(GL_PROJECTION);
+	    glLoadIdentity();
+	    gluPerspective(20, (double)hauteur/largeur, 1, 100000);
+	    glMatrixMode(GL_MODELVIEW);
+	    glLoadIdentity();
+	} else {
+	    glMatrixMode(GL_PROJECTION);
+	    glLoadIdentity();
+	    gluPerspective(70, (double)hauteur/largeur, 1, 100000);
+	    glMatrixMode(GL_MODELVIEW);
+	    glLoadIdentity();
+	    
+	}
+
+	Cube c(0,e.WheelChange() * zoom_y,0,0 ,h.get_w() * h.get_zoom_x(), h.get_w() * h.get_zoom_z());
+	Cube c2(LightPos[0], LightPos[1], LightPos[2], 10,10,10);
+	en.getCamera()->setLookAt(e(), i);
+	en.getCamera()->MovePosition(e, h, c);
+	en.getCamera()->look();
+
+	glLightiv(GL_LIGHT0,GL_POSITION,LightPos);
+	glLightfv(GL_LIGHT0,GL_DIFFUSE,dif); 
+
+	double x = en.getCamera()->target()._X(), y = en.getCamera()->target()._Y(), z = en.getCamera()->target()._Z();
+
+	
+	h.display();
+
+	
+	for(auto it : vec_tree) {
+	    it.display();
+	}
+
+	for ( int i = 0 ; i < position.size() ; i+=3 ) {
+	    glColor3d(1.0,1.0,1.0);
+	    obsf.set_pos(position[i], position[i+1], position[i+2]);
+
+	    obsf.display();
+	}
+	
+
+
+	if ( retour_lum ) {
+	    if ( LightPos[0] > 0 ) {
+		LightPos[0] -= 100;
+		LightPos[2]-= 100; 
+	    } else {
+		retour_lum = false;
+	    }
+	} else {
+	    if ( LightPos[0] < h.get_w() * zoom_x ) {
+		LightPos[0] += 100;
+		LightPos[2] += 100;
+	    } else {
+		retour_lum = true;
+	    }
+	}
+
+
+
+	/*
+	if ( retour ) {
+	    if ( dif[1] > 0.0f ){
+		dif[1] -= 0.05f;
+		dif[2] -= 0.05f;
+	    } else if ( dif[0] > 0.0f ) {
+		dif[0] -= 0.01f;
+	    } else {
+		retour = false;
+	    } 
+	} else {
+	    if ( dif[0] < 0.7f ) {
+		dif[0] += 0.01f;
+		dif[1] += 0.01f;
+		dif[2] += 0.01f;
+	    } else {
+		retour = true;
+	    }
+	}
+	*/
+	c.display();
+	c2.display();
+	stringstream fps;
+	fps << framerate();
+	fps << " X : " << en.getCamera()->position()._X() << " Z :" << en.getCamera()->position()._Z() << " Y : " << en.getCamera()->position()._Y();
+	en.change_title(fps.str().c_str());
+	SDL_GL_SwapBuffers();
+
+
     }
+    return 0;
 }
-
-
-void init (void) 
-{
-    makeCheckImage();
-  glClearColor (0.0, 0.0, 0.0, 0.0); //√àtabli vers quelle couleur la fen√çtre sera vid√àe
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 40,40,0,GL_RGBA, GL_UNSIGNED_BYTE, CheckImage0);
-  // glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-  // glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_TEXTURE_2D);
-}
-
-
-void reshape (int w, int h)
-{
-  glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
-  glMatrixMode (GL_PROJECTION);
-  glLoadIdentity ();
-  gluPerspective(70.0, (GLfloat) w/(GLfloat) h, 0.01, 1000.0);
-  glMatrixMode(GL_MODELVIEW);
-
-}
-
-
-
-void key(unsigned char key,int x,int y) {
-  switch ( key ) {
-  case 'z':
-      front+=2; break;
-  case 's':
-      front -= 2; break;
-  case 'e':
-      angle++; break;
-  case 'a':
-      angle--; break;
-  case 0x1B : exit(0); }
-  MyReDisplay();
-}
-
-void GestionSpecial(int key, int x, int y)
-{printf("vous avez appuy√à sur ");
-  switch (key)
-    {	
-    case GLUT_KEY_F1 : printf("F1 \n");glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-      
-      break;
-    case GLUT_KEY_F2 : printf("F2 \n");glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);break;
-
-    case GLUT_KEY_UP: up+=2; break;
-    case GLUT_KEY_DOWN: up-=2; break;
-	
-    case GLUT_KEY_LEFT : _left+=2;printf("Gauche\n ");
-      break; 
-    case GLUT_KEY_RIGHT : _left-=2; printf("Droite \n");break; 
-    }	
-  printf("position de la souris : ");
-  printf("%d,%d ",x,y);
-  MyReDisplay();
-	
-}	
-
-void Effets(int selection)
-{ 
-	
-  switch (selection)
-    {	
-    case 0 : 	glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);break; 
-    case 1 :glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);break; 
-    case 2 :exit(0);break; 
-    }		
-	
-  MyReDisplay();	
-}
-
-void Menu2(int selection)
-{ 
-  printf("selection Menu2 %i",selection);
-  switch (selection)
-    {	
-    case 0 : 	printf("action toto");break; 
-    case 1:	printf("action titi");break; 	
-    }	
-  MyReDisplay();	
-}
-
-void spinDisplay(void)
-{
-  glutPostRedisplay();
-}
-
-
-
-
-int main(int argc, char** argv)
-{
-
-    
-	
-    glutInit(&argc, argv); 
-
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB  | GLUT_DEPTH ); 
-    glutInitWindowSize (500,500);
-    glutInitWindowPosition (100, 350);
-    glutCreateWindow ("Second Window"); 
-    init ();
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(key);
-    glutSpecialFunc(GestionSpecial);
-    glutDisplayFunc(display); 
-    glutIdleFunc(spinDisplay);
-
-	
-    int sMenu;
-    sMenu = glutCreateMenu(Menu2);
-    glutAddMenuEntry("toto",0);
-    glutAddMenuEntry("titi",1);
-
-    int mainMenu;
-    mainMenu = glutCreateMenu(Effets);
-    glutAddMenuEntry("Mode plein",0);
-    glutAddMenuEntry("Mode fil de fer",1);
-    glutAddMenuEntry("Quitter",2);
-    glutAddSubMenu("Maillages",sMenu);
-
-    glutAttachMenu(GLUT_LEFT_BUTTON);	
-	
-	
-
-    glutMainLoop();
-    return 0;   
-}
-
-
